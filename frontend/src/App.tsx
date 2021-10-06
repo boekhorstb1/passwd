6729ed6439bbbf93303a5af3238d68d5a585ef46
Midah Pasche <midahannep@gmail.com>
2021-10-06 17:15:30 +0200

    use webpack directly to create a single js file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import ChangePasswordForm from './components/ChangePasswordForm';
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,23 +1,59 @@
-const path = require("path")
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
-const glob = require("glob")
+// const path = require("path")
+// const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
+// const glob = require("glob")
 
-    module.exports = {
-      mode: "production",
-      entry: {
-        "bundle.js": glob.sync("build/static/?(js|css)/main.*.?(js|css)").map(f => path.resolve(__dirname, f)),
+//     module.exports = {
+//       mode: "production",
+//       entry: 
+//         // "bundle.js": glob.sync("build/static/?(js|css)/main.*.?(js|css)").map(f => path.resolve(__dirname, f)),
+//         path.resolve(__dirname, 'src/index.tsx')
+//       ,
+//       output: {
+//         path: path.resolve(__dirname, "../js"),
+//         filename: "passwd-react.js",
+//       },
+//       module: {
+//         rules: [
+//           {
+//             test: /\.css$/,
+//             use: ["style-loader", "css-loader"],
+//           },
+//           {
+//             test: /\.tsx?$/,
+//             use: 'ts-loader',
+//             exclude: /node_modules/,
+//           },
+//         ],
+//       },
+//       resolve: {
+//         extensions: ['.tsx', '.ts', '.js'],
+//       },
+//       plugins: [new UglifyJsPlugin()],
+//     }
+
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
       },
-      output: {
-        path: path.resolve(__dirname, "../js"),
-        filename: "passwd-react.js",
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
-      module: {
-        rules: [
-          {
-            test: /\.css$/,
-            use: ["style-loader", "css-loader"],
-          },
-        ],
-      },
-      plugins: [new UglifyJsPlugin()],
-    }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'passwd-react.js',
+    path: path.resolve(__dirname, '../js'),
+  },
+};
